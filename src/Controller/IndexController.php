6f5725c5007652de4cdd77d5ed17fa4b84ad0047
CarlosNavSeg <carlos.raco@hotmail.com>
2023-04-20 16:47:40 +0200

    xd

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Papi;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $repo = $doctrine->getRepository(Papi::class);
+        $papis = $repo->findAll();
+
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'papis' => $papis
         ]);
     }
 }
